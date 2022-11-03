e5c778c6ef98d1dfba4f14a22f32e7f35d07296d
daeuneeee <dxexx2@gmail.com>
2022-11-03 15:54:35 +0900

    [FEATURE]리스트 기능구현TEST

--- src/components/units/crews/list/crewList.container.tsx
+++ src/components/units/crews/list/crewList.container.tsx
@@ -1,7 +1,11 @@
+import { useQuery } from "@apollo/client";
 import CrewListUi from "./crewList.presenter";
+import { FETCH_CREW_BOARDS } from "./crewList.queries";
 
 const CrewList = () => {
-  return <CrewListUi />;
+  const { data } = useQuery(FETCH_CREW_BOARDS);
+  console.log(data);
+  return <CrewListUi data={data} />;
 };
 
 export default CrewList;

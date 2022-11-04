4a4b89b6a4e1973dfe6ac381559022bd5024e19a
daeuneeee <dxexx2@gmail.com>
2022-11-04 21:05:29 +0900

    [Modify]크루작성 퍼블리싱 추가

--- src/components/units/crews/write/crewWrite.container.tsx
+++ src/components/units/crews/write/crewWrite.container.tsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
 import CrewWriteUi from "./crewWrite.presenter";
+import type { Moment } from "moment";
+import type { DatePickerProps } from "antd";
 
 const CrewWrite = () => {
-  return <CrewWriteUi />;
+  const [isOpen, setIsOpen] = useState(false);
+  const [isClicked, setIsClicked] = useState("gender01");
+  const [people, setPeople] = useState(0);
+  const [, setDate] = useState("");
+  const [, setTime] = useState("");
+
+  const onChangeDate: DatePickerProps["onChange"] = (date, dateString) => {
+    console.log(date, dateString);
+    setDate(dateString);
+  };
+  const onChangeTime = (time: Moment | null, timeString: string) => {
+    console.log(time, timeString);
+    setTime(timeString);
+  };
+
+  const onChangeRadio = (event) => {
+    console.log(event);
+    if (event.target.checked) {
+      setIsClicked(event.target.id);
+    }
+  };
+
+  const onToggleModal = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleComplete = (address) => {
+    onToggleModal();
+  };
+
+  const onChangePeople = (event: any) => {
+    setPeople(event);
+  };
+
+  return (
+    <CrewWriteUi
+      onChangeDate={onChangeDate}
+      onChangeTime={onChangeTime}
+      isOpen={isOpen}
+      handleComplete={handleComplete}
+      onToggleModal={onToggleModal}
+      onChangeRadio={onChangeRadio}
+      isClicked={isClicked}
+      onChangePeople={onChangePeople}
+      people={people}
+    />
+  );
 };
 
 export default CrewWrite;

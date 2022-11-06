7eb87347ae1fcf6793f4bd0432032cbfa3e918c8
조정택 <taek@jojeongtaegs-MacBook-Pro.local>
2022-11-06 14:29:01 +0900

    [MODIFY] 채팅페이지 퍼블리싱

--- src/components/units/chat/chat.presenter.tsx
+++ src/components/units/chat/chat.presenter.tsx
@@ -1,112 +1,84 @@
 import * as S from "./chat.styles";
+import { AvatarGroup, Avatar } from "@mui/material";
 
 const ChatUi = () => {
   return (
     <S.Wrapper>
       <S.Container>
-        <S.ChatFList></S.ChatFList>
-        <S.ChatView>
-          <S.ChatGroup>
-            <S.ProfileChatBox>
-              <S.ProfileImg></S.ProfileImg>
-              <S.UserNameChat>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.UserNameChat>
-            </S.ProfileChatBox>
-          </S.ChatGroup>
-          <S.ChatMy>
-            <S.ProfileChatBox>
-              <S.MyChatBox>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.MyChatBox>
-            </S.ProfileChatBox>
-          </S.ChatMy>
-          <S.ChatMy>
-            <S.ProfileChatBox>
-              <S.MyChatBox>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.MyChatBox>
-            </S.ProfileChatBox>
-          </S.ChatMy>
-          <S.ChatMy>
-            <S.ProfileChatBox>
-              <S.MyChatBox>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.MyChatBox>
-            </S.ProfileChatBox>
-          </S.ChatMy>
-          <S.ChatMy>
-            <S.ProfileChatBox>
-              <S.MyChatBox>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.MyChatBox>
-            </S.ProfileChatBox>
-          </S.ChatMy>
-          <S.ChatGroup>
-            <S.ProfileChatBox>
-              <S.ProfileImg></S.ProfileImg>
-              <S.UserNameChat>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.UserNameChat>
-            </S.ProfileChatBox>
-          </S.ChatGroup>
-          <S.ChatGroup>
-            <S.ProfileChatBox>
-              <S.ProfileImg></S.ProfileImg>
-              <S.UserNameChat>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.UserNameChat>
-            </S.ProfileChatBox>
-          </S.ChatGroup>
-          <S.ChatGroup>
-            <S.ProfileChatBox>
-              <S.ProfileImg></S.ProfileImg>
-              <S.UserNameChat>
-                <S.UserName>깜장이</S.UserName>
-                <S.ChatBox>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                  <S.ChatDes>채팅이다. 채팅이다.</S.ChatDes>
-                </S.ChatBox>
-              </S.UserNameChat>
-            </S.ProfileChatBox>
-          </S.ChatGroup>
-        </S.ChatView>
+        <S.ChatContainer>
+          <S.ChatFList>
+            <S.ChatFListTitle>참가중인 크루원</S.ChatFListTitle>
+          </S.ChatFList>
+          <S.ChatView>
+            <S.ChatViewHead>
+              <AvatarGroup max={3}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+                <Avatar
+                  alt="Trevor Henderson"
+                  src="/static/images/avatar/5.jpg"
+                />
+              </AvatarGroup>
+              <S.ChatTitleAndUser>
+                <S.ChattingTitle>채팅 제목이 들어갑니다...</S.ChattingTitle>
+                <S.ChattingUser>
+                  춘딩딩, 땅오, 참가자, 참가자 외 2명
+                </S.ChattingUser>
+              </S.ChatTitleAndUser>
+            </S.ChatViewHead>
+            <S.ChatViewBody>
+              <S.ChatGroup>
+                <S.ProfileChatBox>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 56, height: 56 }}
+                  />
+                  <S.ChatMsgContainer>
+                    <S.GroupChatMsgBox>안녕하세요 ㅎㅎ</S.GroupChatMsgBox>
+                    <S.ChatMstTime>8:00 PM</S.ChatMstTime>
+                  </S.ChatMsgContainer>
+                </S.ProfileChatBox>
+              </S.ChatGroup>
+              <S.ChatGroup>
+                <S.ProfileChatBox>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 56, height: 56 }}
+                  />
+                  <S.ChatMsgContainer>
+                    <S.GroupChatMsgBox>안녕하세요 ㅎㅎ</S.GroupChatMsgBox>
+                    <S.ChatMstTime>8:00 PM</S.ChatMstTime>
+                  </S.ChatMsgContainer>
+                </S.ProfileChatBox>
+              </S.ChatGroup>
+              <S.ChatMy>
+                <S.MyProfileChatBox>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 56, height: 56 }}
+                  />
+                  <S.MyChatMsgContainer>
+                    <S.MyGroupChatMsgBox>
+                      다들 어디쯤 오셨나요??
+                    </S.MyGroupChatMsgBox>
+                    <S.ChatMstTime>8:00 PM</S.ChatMstTime>
+                  </S.MyChatMsgContainer>
+                </S.MyProfileChatBox>
+              </S.ChatMy>
+            </S.ChatViewBody>
+            <S.ChatViewFoot>
+              <S.ChatForm>
+                <S.ChatInput type="text" placeholder="채팅을 입력해주세요." />
+                <S.ChatBtn>전송</S.ChatBtn>
+              </S.ChatForm>
+            </S.ChatViewFoot>
+          </S.ChatView>
+        </S.ChatContainer>
       </S.Container>
     </S.Wrapper>
   );

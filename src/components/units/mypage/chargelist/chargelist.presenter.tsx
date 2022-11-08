15fd358e45ee93d7f127c0f113d41a5678b30c83
조정택 <taek@jojeongtaegs-MacBook-Pro.local>
2022-11-08 21:39:39 +0900

    [FEATURE] 마이페이지 충전내역/사용내역 반응형 퍼블리싱

--- src/components/units/mypage/chargelist/chargelist.presenter.tsx
+++ src/components/units/mypage/chargelist/chargelist.presenter.tsx
@@ -2,6 +2,7 @@ import MyPageNav from "../navigation";
 import * as S from "./chargelist.styles";
 import * as A from "../applylist/applylist.styles";
 import { Pagination } from "antd";
+import "antd/dist/antd.css";
 
 const ChargeListUi = () => {
   return (
@@ -10,29 +11,13 @@ const ChargeListUi = () => {
       <S.Container>
         <S.ChargeContainer>
           <S.TitleUl>
-            <S.ListLiNum>No.</S.ListLiNum>
+            <S.ListLiNum className="mobile">No.</S.ListLiNum>
             <S.ListLiDate>날짜</S.ListLiDate>
             <S.ListLiChargePoint>충전금액</S.ListLiChargePoint>
             <S.ListLiSign>환불하기</S.ListLiSign>
           </S.TitleUl>
           <S.ContentUl>
-            <S.ListLiNum>1</S.ListLiNum>
-            <S.ListLiDate>2022.01.01</S.ListLiDate>
-            <S.ListLiChargePoint>20,000P</S.ListLiChargePoint>
-            <S.ListLiSign>
-              <S.RefundBtn>환불하기</S.RefundBtn>
-            </S.ListLiSign>
-          </S.ContentUl>
-          <S.ContentUl>
-            <S.ListLiNum>1</S.ListLiNum>
-            <S.ListLiDate>2022.01.01</S.ListLiDate>
-            <S.ListLiChargePoint>20,000P</S.ListLiChargePoint>
-            <S.ListLiSign>
-              <S.RefundBtn>환불하기</S.RefundBtn>
-            </S.ListLiSign>
-          </S.ContentUl>
-          <S.ContentUl>
-            <S.ListLiNum>1</S.ListLiNum>
+            <S.ListLiNum className="mobile">1</S.ListLiNum>
             <S.ListLiDate>2022.01.01</S.ListLiDate>
             <S.ListLiChargePoint>20,000P</S.ListLiChargePoint>
             <S.ListLiSign>

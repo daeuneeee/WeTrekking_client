8ef8af55e5cee31c4abb91b06fa505b4ec959325
조정택 <taek@jojeongtaegs-MacBook-Pro.local>
2022-11-14 17:50:23 +0900

    [MODIFY] PWA 기능 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,17 +6,6 @@ import ApolloSetting from "../src/components/apollo";
 import Layout from "../src/components/commons/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("service worker registration successful");
-      })
-      .catch((err) => {
-        console.warn("service worker registration failed", err.message);
-      });
-  }
-
   return (
     <>
       <noscript>You should use javascript</noscript>

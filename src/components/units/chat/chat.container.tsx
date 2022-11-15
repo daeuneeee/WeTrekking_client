2b5754cfa4fe86dffb840bf8d490fb78ac0f0719
조정택 <taek@jojeongtaegs-MacBook-Pro.local>
2022-11-15 11:02:52 +0900

    [Feature] 채팅 기능

--- src/components/units/chat/chat.container.tsx
+++ src/components/units/chat/chat.container.tsx
@@ -1,6 +1,25 @@
 import ChatUi from "./chat.presenter";
+// import io from "socket.io-client";
+// import { ChangeEvent, useState } from "react";
 
 const Chat = () => {
+  // const [chatMsg, setChatMsg] = useState("");
+
+  // const socket = io.connect("http://localhost:3001");
+
+  // const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+  //   setChatMsg(event.target.value);
+  // };
+
+  // const onClickChatBtn = () => {
+  //   socket.emit("send_message", chatMsg);
+
+  //   const aaa = socket.on("msg", (msg: any) => {
+  //     console.log(msg);
+  //   });
+  //   console.log(aaa);
+  // };
+
   return <ChatUi />;
 };
 

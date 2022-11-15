28da6213c3bd56509df5620477dd0c387b945344
조정택 <taek@jojeongtaegs-MacBook-Pro.local>
2022-11-15 11:16:05 +0900

    [FEATURE] 마이페이지 찜 리스트 기능

---
+++ src/components/units/mypage/pickedlist/picked.queries.ts
@@ -0,0 +1,44 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_DIBS = gql`
+  query fetchDibs {
+    fetchDibs {
+      id
+      user {
+        id
+        email
+        name
+        nickname
+        birth
+        phone
+        gender
+        profile_img
+      }
+      crewBoard {
+        id
+        title
+        description
+        date
+        dateTime
+        address
+        addressDetail
+        gender
+        dues
+        peoples
+        thumbnail
+        mountain
+        createdAt
+        user {
+          id
+          email
+          name
+          nickname
+          birth
+          phone
+          gender
+          profile_img
+        }
+      }
+    }
+  }
+`;
---
+++ src/components/units/mypage/pickedlist/picked.types.ts
@@ -0,0 +1,5 @@
+import { IQuery } from "../../../../commons/types/generated/types";
+
+export interface IPickedListUiProps {
+  data?: Pick<IQuery, "fetchDibs">;
+}
